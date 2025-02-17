0e4988e8c2b5acbde7058f74648273ebcbff9ae2
Your Name <your-email@example.com>
2025-02-17 11:01:15 +0530

    Project created

--- Api/models/category.js
+++ Api/models/category.js
@@ -19,6 +19,7 @@ const CategorySchema = new mongoose.Schema({
         type: Number,
         default: 0
     },
+    
    gatePass: [{ type: mongoose.Schema.Types.ObjectId, ref: 'gatePass' }]
 });
 
